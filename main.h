686dafe72ce09d5263e9df58246d9135bfa43ff2
haithemyk <haithemyk0707@gmail.com>
2023-04-22 18:08:29 +0100

    first task first commit

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+/* Declare function prototypes */
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
