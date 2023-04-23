d126249553b7bd8cf0ffe2356a6801f45af5925f
Haithem Youcef Khoudja <111178558+haithemyk@users.noreply.github.com>
2023-04-23 14:35:17 +0100

    Update main.h
    Resolve problems

--- main.h
+++ main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
-
 #include "stdio.h"
 #include "stdarg.h"
 #include "stdlib.h"
@@ -10,13 +8,3 @@
 int _printf(const char *format, ...);
 
 #endif
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-/* Declare function prototypes */
-int _printf(const char *format, ...);
-
-#endif /* MAIN_H */
->>>>>>> a007ffd9286a7a14be121d500743e697391a18b9
